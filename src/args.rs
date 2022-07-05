bb89611e5fd8c71fe6189c1379501c9cc4663dd3
Vipershroom <undertalefan812@gmail.com>
2022-07-05 02:15:38 -0400

    Add basic parser

---
+++ src/args.rs
@@ -0,0 +1,12 @@
+use clap::Parser;
+use std::path::{Path, PathBuf};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about=None)]
+pub struct Args {
+    #[clap(value_parser)]
+    pub directory: Option<PathBuf>,
+
+    #[clap(short, value_parser, default_value_t = false)]
+    pub prog: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+mod args;
+
 fn main() {
-    println!("Hello, world!");
+    let args = args::Args::parse();
+
+    println!(
+        "Your args are: {:?}, {:?}",
+        args.directory.unwrap(),
+        args.prog
+    )
 }
