75bcb43621d8eaae48c036ef06dfac2670140f53
Vipershroom <undertalefan812@gmail.com>
2022-08-05 18:49:19 -0400

    It works

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,11 @@
 use clap::Parser;
+use std::path::{Path, PathBuf};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about=None)]
 pub struct Args {
     #[clap(value_parser)]
-    pub directory: Option<String>,
+    pub directory: Option<PathBuf>,
 
     #[clap(short, value_parser, default_value_t = false)]
     pub flag: bool,
--- src/file.rs
+++ src/file.rs
@@ -1,17 +1,27 @@
+use crate::date::EndDate;
 use fs_extra::dir::copy;
 use fs_extra::dir::CopyOptions;
 use std::error::Error;
 use std::fs;
+use std::path::{Path, PathBuf};
 
-use crate::date::EndDate;
-
-pub fn rename_file(date_struct: EndDate, dir: String, flag: bool) -> Result<(), Box<dyn Error>> {
+#[allow(unused_assignments)]
+pub fn rename_file(date_struct: EndDate, dir: PathBuf, flag: bool) -> Result<(), Box<dyn Error>> {
     let options = CopyOptions::new();
 
-    copy("WE_Template", &dir, &options)?;
+    let mut copy_dir = PathBuf::new();
 
-    let form1 = format!("{}\\WE_Template", &dir);
-    let form2 = format!("{}\\{}", dir, date_struct.format_week());
+    if flag {
+        copy_dir.push("./templates/WE_Template_prog/");
+    } else {
+        copy_dir.push("./templates/WE_Templater_web/");
+    }
+    copy(&copy_dir, &dir, &options).unwrap();
+
+    let copy_slice = &copy_dir.to_str().unwrap()[12..];
+
+    let form1 = dir.join(PathBuf::from(copy_slice));
+    let form2 = dir.join(date_struct.format_week());
 
     fs::rename(form1, form2)?;
 
